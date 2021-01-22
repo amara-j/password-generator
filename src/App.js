c82b69cd6009e8eec950d895cecb0af4b4470aba
Amara Jaeger <amara.jaeger@gmail.com>
2021-01-22 16:39:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const PasswordView = () => {
+const [password, updatePassword] = useState("");
+
+const generatePassword = () => {
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let newPassword = []
+for (let i = 0; i< 10; i++){
+  newPassword.push(alphabet[Math.floor(26*Math.random())])
+newPassword.join("");
+}
+updatePassword(newPassword);
+  }
+
+  return(
+    <div className = "passwordView">
+    {password}
+     <button onClick={ () => generatePassword()}>Click to generate password</button>
+    </div>
+   
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Secure Password Generator
       </header>
-    </div>
+      <PasswordView/>
+     </div>
   );
 }
-
 export default App;
