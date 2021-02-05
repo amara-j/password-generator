b73fba581980ace5d0b7772c1b45960198411be9
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-05 12:14:42 -0500

    fixed generatePassword function so that when symbols/numbers are included, they arent just possible but certain.  password doesnt yet randomize so digits and symbols always come first

--- src/App.js
+++ src/App.js
@@ -41,29 +41,46 @@ const PasswordView = () => {
     updateDigitChange(!includeDigit);
   };
 
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  const randomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
   const generatePassword = () => {
     const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
       ""
     );
     const digits = "0123456789".split("");
     const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
-    let passwordCharSet = [];
-    if (includeDigit && includeSymbol) {
-      passwordCharSet = alphabet.concat(digits.concat(symbols));
-    } else if (includeDigit) {
-      passwordCharSet = alphabet.concat(digits);
-    } else if (includeSymbol) {
-      passwordCharSet = alphabet.concat(symbols);
-    } else if (!includeDigit && !includeSymbol) {
-      passwordCharSet = alphabet;
-    }
+
     let newPassword = [];
-    for (let i = 0; i < sliderValue; i++) {
-      newPassword.push(
-        passwordCharSet[Math.floor(passwordCharSet.length * Math.random())]
-      );
-      newPassword.join("");
+    let numberOfDigits = 0;
+    let numberOfSymbols = 0;
+    if (includeDigit) {
+      numberOfDigits = randomInt(1, sliderValue / 3);
     }
+    if (includeSymbol) {
+      numberOfSymbols = randomInt(1, sliderValue / 3);
+    }
+
+    for (let i = 0; i < numberOfDigits; i++) {
+      newPassword.push(digits[Math.floor(digits.length * Math.random())]);
+    }
+    for (let j = 0; j < numberOfSymbols; j++) {
+      newPassword.push(symbols[Math.floor(symbols.length * Math.random())]);
+    }
+    for (let k = 0; k < sliderValue - numberOfDigits - numberOfSymbols; k++) {
+      newPassword.push(alphabet[Math.floor(alphabet.length * Math.random())]);
+    }
+    newPassword.join("");
     handleGeneratePasswordClick(true);
     updatePassword(newPassword);
   };
@@ -90,7 +107,7 @@ const PasswordView = () => {
       </div>
       <form>
         <label>
-          Include symbols
+          Allow symbols
           <input
             type="checkbox"
             defaultChecked="true"
@@ -98,7 +115,7 @@ const PasswordView = () => {
           />
         </label>
         <label>
-          Include digits
+          Allow digits
           <input
             type="checkbox"
             defaultChecked="true"
@@ -113,7 +130,7 @@ const PasswordView = () => {
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">Secure Password Generator</header>
+      <header className="App-header">Password Generator</header>
       <PasswordView />
     </div>
   );
